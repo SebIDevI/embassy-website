f8550facffa9b160fd24856076a7b79759b9e9c7
SebIDevI <71215836+SebIDevI@users.noreply.github.com>
2024-04-20 12:50:15 +0300

    Added kinetoLin to chiropractor.ro

--- app/chiropractor.ro/Slider.tsx
+++ app/chiropractor.ro/Slider.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import Image from "next/image";
 
 import OsteopathLogo from "@/public/logo osteopath concept black.png";
+import KinetoLogo from "@/public/Logo KinetoLin.png";
 
 function Slider() {
   return (
     <>
       <h5 className="text-lg font-graphik text-center uppercase">
-        Partener oficial
+        Parteneri oficiali
       </h5>
       <div className="flex justify-center w-full h-[10vh] sm:h-[20vh] min-h-[50px] md:h-[20vh] md:min-h-[200px] relative my-20">
         <div className="w-full h-full overflow-hidden relative flex">
@@ -16,6 +17,11 @@ function Slider() {
               src={OsteopathLogo}
               alt="Chiropractor Logo"
               className="md:w-[200px] w-5/6 mx-auto h-auto object-fit md:mx-[40px] filter opacity-80"
+            />
+            <Image
+              src={KinetoLogo}
+              alt="Chiropractor Logo"
+              className="md:w-[200px] w-5/6 mx-auto h-auto object-fit md:mx-[40px] filter opacity-70"
             />
           </div>
         </div>
