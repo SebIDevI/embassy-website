910b94fb2d053d2b8abe876f8894322ac7dcb03b
SebIDevI <71215836+SebIDevI@users.noreply.github.com>
2024-10-08 21:07:24 +0300

    added logo for bodyadjustments to chiropractor.ro

--- app/chiropractor.ro/Slider.tsx
+++ app/chiropractor.ro/Slider.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 
 import OsteopathLogo from "@/public/logo osteopath concept black.png";
 import KinetoLogo from "@/public/Logo KinetoLin.png";
+import Bodyadjustment from "@/public/White_Bodyadjustment.png";
 
 function Slider() {
   return (
@@ -10,9 +11,9 @@ function Slider() {
       <h5 className="text-lg font-graphik text-center uppercase">
         Parteneri oficiali
       </h5>
-      <div className="flex justify-center w-full h-[10vh] sm:h-[20vh] min-h-[50px] md:h-[20vh] md:min-h-[200px] relative my-20">
+      <div className="flex justify-center w-full h-[20vh] min-h-[50px] md:min-h-[200px] relative my-20">
         <div className="w-full h-full relative flex">
-          <div className="flex justify-center md:gap-20 gap-5 items-center w-full h-full absolute left-1/2 top-0 -translate-x-1/2">
+          <div className="flex flex-col sm:flex-row justify-center md:gap-20 gap-5 items-center w-full h-full absolute left-1/2 top-0 -translate-x-1/2">
             <div className="w-auto h-full max-w-[200px] flex justify-center items-center">
               <Image
                 src={OsteopathLogo}
@@ -25,6 +26,13 @@ function Slider() {
                 src={KinetoLogo}
                 alt="Chiropractor Logo"
                 className="max-w-[200px] w-[90%] mx-auto h-auto object-fit md:mx-[40px] filter opacity-70"
+              />
+            </div>
+            <div className="w-auto h-full max-w-[200px] flex justify-center items-center">
+              <Image
+                src={Bodyadjustment}
+                alt="Chiropractor Logo"
+                className="max-w-[200px] w-[90%] mx-auto h-auto object-fit md:mx-[40px] filter opacity-70 invert"
               />
             </div>
           </div>
