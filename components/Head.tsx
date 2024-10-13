34bf7259ab19811b48e16a3a4d27b3f6b48da4bd
Catrina Andrei Sebastian <71215836+SebIDevI@users.noreply.github.com>
2024-10-13 17:10:35 +0300

    Update Head.tsx changs to sizes 3

--- components/Head.tsx
+++ components/Head.tsx
@@ -19,8 +19,8 @@ function Head() {
     // bg: bg-[linear-gradient(90deg,#e8f1f5,#fafafa_100%)]
     <section className="h-[100vh] min-h-[700px] lg:block flex items-center justify-center relative lg:pt-10 bg-[#ecf3f8]">
       <div className="flex flex-col-reverse lg:flex-row lg:items-center container">
-        <div className="font-extrabold text-xl w-full text-center lg:text-left lg:text-2xl">
-          <h1 className="text-5xl sm:text-6xl lg:text-8xl xl:text-7xl text-blueEmb font-pro overflow-hidden scale-[1.2]">
+        <div className="font-extrabold text-2xl w-full text-center lg:text-left lg:text-xl">
+          <h1 className="text-5xl sm:text-6xl lg:text-8xl xl:text-8xl text-blueEmb font-pro overflow-hidden">
             <span className="translate-y-[200%] py-4 inline-block animate-ttlSld delay-7">
               Embassy
             </span>
