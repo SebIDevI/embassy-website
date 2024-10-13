3e91169f7298ddd12f21a30de3ae7879e22c433d
Catrina Andrei Sebastian <71215836+SebIDevI@users.noreply.github.com>
2024-10-13 17:41:26 +0300

    Update CarouselContainer.tsx web modifies for carousel v1

--- app/web/CarouselContainer.tsx
+++ app/web/CarouselContainer.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { Carwsel } from "./Carwsel";
+import osteopath from "@/public/osteopath-firstPage.png";
+import pirelli from "@/public/pirelli-website.jpeg";
+import rhyminem from "@/public/rhyminem.png";
 
 function CarouselContainer() {
+  
+  const imgs = [pirelli, osteopath, rhyminem];
+  
   return (
     <div className="md:w-full w-[80%] mx-auto md:mx-0 flex items-center flex-col justify-center gap-10">
-      <Carwsel />
+{/*       <Carwsel /> */}
+      <div className="grid grid-cols-3 grid-rows-6">
+        <div className="w-full h-full bg-gray-200"></div>
+        <div className="w-full h-full bg-gray-200 row-span-2"></div>
+        <div className="w-full h-full bg-gray-200"></div>
+        <div className="w-full h-full bg-gray-200 row-span-2"></div>
+        <div className="w-full h-full bg-gray-200"></div>
+        <div className="w-full h-full bg-gray-200 row-span-2"></div>
+      </div>
     </div>
   );
 }
