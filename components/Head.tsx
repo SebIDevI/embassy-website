7e66285b76d8400ca22f2882ae916eae823c4f47
Catrina Andrei Sebastian <71215836+SebIDevI@users.noreply.github.com>
2024-10-13 15:23:17 +0300

    Update Head.tsx text changes 2

--- components/Head.tsx
+++ components/Head.tsx
@@ -20,7 +20,7 @@ function Head() {
     <section className="h-[100vh] min-h-[700px] lg:block flex items-center justify-center relative lg:pt-10 bg-[#ecf3f8]">
       <div className="flex flex-col-reverse lg:flex-row lg:items-center container">
         <div className="font-extrabold text-xl w-full text-center lg:text-left lg:text-2xl">
-          <h1 className="text-3xl sm:text-6xl lg:text-8xl xl:text-7xl text-blueEmb font-pro overflow-hidden">
+          <h1 className="text-5xl sm:text-6xl lg:text-8xl xl:text-7xl text-blueEmb font-pro overflow-hidden">
             <span className="translate-y-[200%] py-4 inline-block animate-ttlSld delay-7">
               Embassy
             </span>
