2029445ec3e6514854b3516ca9e42a3024aab16d
Catrina Andrei Sebastian <71215836+SebIDevI@users.noreply.github.com>
2024-10-13 15:30:29 +0300

    Update Head.tsx scaling try 2

--- components/Head.tsx
+++ components/Head.tsx
@@ -19,13 +19,13 @@ function Head() {
     // bg: bg-[linear-gradient(90deg,#e8f1f5,#fafafa_100%)]
     <section className="h-[100vh] min-h-[700px] lg:block flex items-center justify-center relative lg:pt-10 bg-[#ecf3f8]">
       <div className="flex flex-col-reverse lg:flex-row lg:items-center container">
-        <div className="font-extrabold text-xl w-full text-center lg:text-left lg:text-2xl scale-[1.2]">
-          <h1 className="text-5xl sm:text-6xl lg:text-8xl xl:text-7xl text-blueEmb font-pro overflow-hidden">
+        <div className="font-extrabold text-xl w-full text-center lg:text-left lg:text-2xl">
+          <h1 className="text-5xl sm:text-6xl lg:text-8xl xl:text-7xl text-blueEmb font-pro overflow-hidden scale-[1.2]">
             <span className="translate-y-[200%] py-4 inline-block animate-ttlSld delay-7">
               Embassy
             </span>
           </h1>
-          <h3 className="mb-7 font-pro overflow-hidden">
+          <h3 className="mb-7 font-pro overflow-hidden scale-[1.2]">
             <span className="translate-y-[200%] py-2 inline-block animate-ttlSld delay-9">
               OFFICE FOR CREATIVE DIRECTION
             </span>
