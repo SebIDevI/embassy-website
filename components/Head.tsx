84f51b8afa563896c8ddb4c43bf3255ad9b09255
Catrina Andrei Sebastian <71215836+SebIDevI@users.noreply.github.com>
2024-10-13 15:27:22 +0300

    Update Head.tsx scaling up

--- components/Head.tsx
+++ components/Head.tsx
@@ -19,7 +19,7 @@ function Head() {
     // bg: bg-[linear-gradient(90deg,#e8f1f5,#fafafa_100%)]
     <section className="h-[100vh] min-h-[700px] lg:block flex items-center justify-center relative lg:pt-10 bg-[#ecf3f8]">
       <div className="flex flex-col-reverse lg:flex-row lg:items-center container">
-        <div className="font-extrabold text-xl w-full text-center lg:text-left lg:text-2xl">
+        <div className="font-extrabold text-xl w-full text-center lg:text-left lg:text-2xl scale-[1.2]">
           <h1 className="text-5xl sm:text-6xl lg:text-8xl xl:text-7xl text-blueEmb font-pro overflow-hidden">
             <span className="translate-y-[200%] py-4 inline-block animate-ttlSld delay-7">
               Embassy
@@ -58,7 +58,7 @@ function Head() {
               controls={false}
               controlsList="nofullscreen nodownload noremoteplayback noplaybackrate foobar"
               disablePictureInPicture
-              style={{ width: "100%", height: "100%", objectFit: "contain" }}
+              style={{ width: "100%", height: "100%", objectFit: "contain", scale: "1.2" }}
             >
               <source
                 src="../Embassy network_triangle Animation.mp4"
@@ -70,7 +70,6 @@ function Head() {
               videoSrc="../Embassy network_triangle Animation.mp4"
               poster="/videoz/hero_image 926px.png"
               alt="Video Marafaka"
-              className="scale-[1.2]"
             />
           </div>
         </div>
